3c3ed3aeffda1349df61f88a31687261f6cfd7ed
MD-Muddabir <mohd.muddabiruszama@gmail.com>
2025-09-29 19:10:04 +0530

    Create the new app.ts file and add the the working buttons

---
+++ app.ts
@@ -0,0 +1,10 @@
+const number1 = document.getElementById("num1") as HTMLInputElement
+const number2 = document.getElementById("num2") as HTMLInputElement
+
+const Addbtn = document.getElementById("Add") as HTMLButtonElement
+const Subbtn = document.getElementById("Sub") as HTMLButtonElement
+const Multibtn = document.getElementById("Multi") as HTMLButtonElement
+const Divbtn = document.getElementById("Divide") as HTMLButtonElement
+
+const printResult = document.getElementById("result") as HTMLOutputElement
+
