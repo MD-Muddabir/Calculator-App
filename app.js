ba5ac8e05e8c924fdb368f01c9357cf154cbfc5a
MD-Muddabir <mohd.muddabiruszama@gmail.com>
2025-09-29 19:41:03 +0530

    I have  Modified the function.

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ var number1 = document.getElementById("num1");
 var number2 = document.getElementById("num2");
 var Addbtn = document.getElementById("Add");
 var Subbtn = document.getElementById("Sub");
-var Multibtn = document.getElementById("Multi");
+var Multibtn = document.getElementById("Mult");
 var Divbtn = document.getElementById("Divide");
 var printResult = document.getElementById("result");
 function AddNumbers() {
     var a = parseFloat(number1.value);
-    var B = parseFloat(number2.value);
-    var result = a + B;
+    var b = parseFloat(number2.value);
+    var result = a + b;
     printResult.textContent = result.toString();
 }
+Addbtn.addEventListener("click", AddNumbers);
+function SubtractNumbers() {
+    var a = parseFloat(number1.value);
+    var b = parseFloat(number2.value);
+    var result = a - b;
+    printResult.textContent = result.toString();
+}
+Subbtn.addEventListener("click", SubtractNumbers);
+function MultiplyNumbers() {
+    var a = parseFloat(number1.value);
+    var b = parseFloat(number2.value);
+    var result = a * b;
+    printResult.textContent = result.toString();
+}
+Multibtn.addEventListener("click", MultiplyNumbers);
+function DivideNumbers() {
+    var a = parseFloat(number1.value);
+    var b = parseFloat(number2.value);
+    var result = a / b;
+    printResult.textContent = result.toString();
+}
+Divbtn.addEventListener("click", DivideNumbers);
--- app.ts
+++ app.ts
@@ -3,16 +3,44 @@ const number2 = document.getElementById("num2") as HTMLInputElement
 
 const Addbtn = document.getElementById("Add") as HTMLButtonElement
 const Subbtn = document.getElementById("Sub") as HTMLButtonElement
-const Multibtn = document.getElementById("Multi") as HTMLButtonElement
+const Multibtn = document.getElementById("Mult") as HTMLButtonElement
 const Divbtn = document.getElementById("Divide") as HTMLButtonElement
 
 const printResult = document.getElementById("result") as HTMLOutputElement
 
 function AddNumbers() {
     let a = parseFloat(number1.value);
-    let B = parseFloat(number2.value);
-    let result = a + B;
+    let b = parseFloat(number2.value);
+    let result = a + b;
     printResult.textContent = result.toString();
 }
 
 Addbtn.addEventListener("click", AddNumbers)
+
+function SubtractNumbers() {
+    let a = parseFloat(number1.value);
+    let b = parseFloat(number2.value);
+    let result = a - b;
+    printResult.textContent = result.toString();
+}
+
+Subbtn.addEventListener("click", SubtractNumbers);
+
+
+function MultiplyNumbers() {
+    let a = parseFloat(number1.value);
+    let b = parseFloat(number2.value);
+    let result = a * b;
+    printResult.textContent = result.toString();
+}
+
+Multibtn.addEventListener("click", MultiplyNumbers);
+
+function DivideNumbers() {
+    let a = parseFloat(number1.value);
+    let b = parseFloat(number2.value);
+    let result = a / b;
+    printResult.textContent = result.toString();
+}
+
+Divbtn.addEventListener("click", DivideNumbers);
